6c769d0d818217c4c73313396b42d02e9509ad84
Daniel Ball <daniel.ball@ext.ons.gov.uk>
2025-04-01 08:03:56 +0100

    Try using FREEZER_RELATIVE_URLS to see if it solves path problems for CSS

--- freeze.py
+++ freeze.py
@@ -3,6 +3,8 @@ from app import create_app
 
 app = create_app()
 app.config["FREEZER_DESTINATION"] = "../build"
+app.config["FREEZER_RELATIVE_URLS"] = True
+
 freezer = Freezer(app)
 
 if __name__ == "__main__":
